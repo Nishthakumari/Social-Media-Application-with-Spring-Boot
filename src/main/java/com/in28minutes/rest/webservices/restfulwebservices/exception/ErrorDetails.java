3d82614b092d24a7103759e9ea44a7610c188088
Nishthakumari <knishtha0@gmail.com>
2023-04-13 15:10:45 +0530

    Implementing Generic Exception Handling for all Resources

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ErrorDetails.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -1,19 +1,19 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ErrorDetails {
-    private LocalDate timestamp;
+    private LocalDateTime timestamp;
     private String message;
     private String details;
 
-    public ErrorDetails(LocalDate timestamp, String message, String details) {
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
         this.timestamp = timestamp;
         this.message = message;
         this.details = details;
     }
 
-    public LocalDate getTimestamp() {
+    public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
