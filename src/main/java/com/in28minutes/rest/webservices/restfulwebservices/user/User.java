2e760f8bb6496945a875d4212fa5ea52d5d991be
Nishthakumari <knishtha0@gmail.com>
2023-04-15 16:43:51 +0530

    Creating User Entity and some test data

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,20 +1,30 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+
+    protected User() {
+
+    }
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
-    @Size(min = 2)
-    @JsonProperty("user_name")
+    @Size(min=2, message = "Name should have atleast 2 characters")
+    //@JsonProperty("user_name")
     private String name;
 
-    @Past
-    @JsonProperty("birth_date")
+    @Past(message = "Birth Date should be in the past")
+    //@JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
